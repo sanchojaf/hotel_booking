f1fd43ec624b725e8344fa8f7aeb9ae1b3058835
Miguel Sancho <sanchojaf@gmail.com>
2014-03-21 11:34:23 -0400

    In the first step in the measurement set workflow select a customer pre-existented

--- app/models/spree/user_decorator.rb
+++ app/models/spree/user_decorator.rb
@@ -4,6 +4,10 @@ module Spree
     has_many :customers, :through => :measurement_sets #if role is seller
     has_many :sellers, :through => :measurement_sets # if role is customer
 
+    def name      
+      return "#{ship_address.first_name} #{ship_address.last_name}" if ship_address  
+    end
+
     has_attached_file :icon,
       styles: { mini:'<32x32>', normal: '<250x250>' },
       default_style: :normal,
